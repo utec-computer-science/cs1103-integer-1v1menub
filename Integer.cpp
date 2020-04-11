c595cf2b7618f15a0cd57886e78e7525375f9f5d
1v1menub <ignacio.rubio@utec.edu.pe>
2020-04-11 06:50:10 -0500

    xd4?

--- Integer.cpp
+++ Integer.cpp
@@ -5,15 +5,14 @@
 #include "Integer.h"
 
 Integer::Integer() {
-
 }
 
-Integer::Integer(int value) {
-
+Integer::Integer(int _value) {
+    value = _value;
 }
 
 Integer::Integer(const Integer &other) {
-
+    value = other.value;
 }
 
 Integer Integer::operator+(const Integer &other) {
@@ -28,8 +27,4 @@ Integer::operator int() {
     return 0;
 }
 
-//NO ME ACORDABA NADA DE SOBRECARGA DE OPERADORES. VI TODOS LOS VIDEOS QUE PUDE/ LEI TUTORIALES EN INTERNET
-//Y ELLOS LO HACIAN DIFERENTE O CON STRUCT
-//NO SE COMO HACER PARA PODER ACCEDER AL VALUE Y TMB ME TIRA ERRORES EN LOS PARAMETROS DE LA SOBRECARGA IDK
-//QUIERO VER COMO SE HACE EN LA CLASE. ESTE ES MI UNICO DESEO ANTES DE MORIR
-
+//ESTOY CONFUNDIDO NO SE COMO HACER ESTO
