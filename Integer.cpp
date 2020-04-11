54fb56d5c5860f7ecc286a644d249fdcf81f641c
1v1menub <ignacio.rubio@utec.edu.pe>
2020-04-11 06:19:48 -0500

    xd?

--- Integer.cpp
+++ Integer.cpp
@@ -1,1 +1,33 @@
+//
+// Created by CABRIGI on 4/11/2020.
+//
+
+#include "Integer.h"
+
+Integer::Integer() {
+}
+
+Integer::Integer(int _value) {
+    value = _value;
+}
+
+Integer::Integer(const Integer& other) {
+    value = other.value;
+}
+
+Integer operator=(const Integer& other) {
+
+}
+
+Integer operator+(int x) {
+}
+
+
+Integer::operator int() {
+
+}
+
+//NO ME ACORDABA NADA DE SOBRECARGA DE OPERADORES. VI TODOS LOS VIDEOS QUE PUDE/ LEI TUTORIALES EN INTERNET
+//Y ELLOS LO HACIAN DIFERENTE O CON STRUCT
+//NO SE COMO HACER PARA PODER ACCEDER AL VALUE Y TMB ME TIRA ERRORES EN LOS PARAMETROS DE LA SOBRECARGA IDK
 
--- Integer.h
+++ Integer.h
@@ -1,1 +1,24 @@
+//
+// Created by CABRIGI on 4/11/2020.
+//
 
+#ifndef POO2_TEO_IMPLEMENTING_INTEGER_INTEGER_H
+#define POO2_TEO_IMPLEMENTING_INTEGER_INTEGER_H
+
+#include <iostream>
+
+class Integer {
+    int value;
+public:
+    Integer();
+    Integer(int value);
+    Integer(const Integer& other);
+    Integer operator+(const Integer& other);
+    Integer operator=(int x);
+    operator int();
+    friend std::ostream& operator <<(std::ostream os, const Integer&);
+    friend std::istream& operator >>(std::istream os, Integer&);
+};
+
+
+#endif //POO2_TEO_IMPLEMENTING_INTEGER_INTEGER_H
